7b3802c90d6e58c57a9efdcbeaf566e3cd87c7e4
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2025-03-05 22:00:44 +0700

    P2_Prak3_Menampilkan View dari Controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    
+    public function hello(){
+        return('Hello World');
+    } 
+    public function greeting(){
+        return view('blog.hello', ['name' => 'Okta']); 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,5 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function () { 
-    return view('blog.hello', ['name' => 'Okta']); 
-}); 
+Route::get('/greeting', [WelcomeController::class,
+'greeting']);
